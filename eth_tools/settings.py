070bd134042cada2e27591f37de1cb40bfcafaae
mkiefferus <kiema745@gmail.com>
2024-11-11 17:04:39 +0100

    fix data output path

--- eth_tools/settings.py
+++ eth_tools/settings.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
 
-DEFAULT_OUTPUT_DIR = Path(".data")
+BASE_DIR = Path(__file__).resolve().parent.parent
+DEFAULT_OUTPUT_DIR = Path(os.path.join(BASE_DIR, ".data"))
 
 # Allocations
 ROOMS_DIR = Path(os.path.join(DEFAULT_OUTPUT_DIR, "room_allocations"))
